ecf720d209bf0158dfbd85953095eeafdb4ef563
Jason Goldberger <jlgoldb2@asu.edu>
2019-04-22 10:00:06 -0700

    fixed? and test

--- test/and_test.exs
+++ test/and_test.exs
@@ -13,13 +13,13 @@ defmodule Annex.AndTest do
   ]
 
   @labels [
-    [1.0, 0.0],
-    [0.0, 1.0],
-    [0.0, 1.0],
-    [0.0, 1.0],
-    [0.0, 1.0],
-    [0.0, 1.0],
-    [0.0, 1.0]
+    [1.0],
+    [0.0],
+    [0.0],
+    [0.0],
+    [0.0],
+    [0.0],
+    [0.0]
   ]
 
   test "and works" do
@@ -27,7 +27,9 @@ defmodule Annex.AndTest do
              Annex.sequence([
                Annex.dense(6, input_dims: 3),
                Annex.activation(:tanh),
-               Annex.dense(2, input_dims: 6),
+               Annex.dense(6, input_dims: 6),
+               Annex.activation(:tanh),
+               Annex.dense(1, input_dims: 6),
                Annex.activation(:sigmoid)
              ])
              |> Annex.train(@data, @labels,
@@ -36,9 +38,10 @@ defmodule Annex.AndTest do
                halt_condition: {:epochs, 100_000}
              )
 
-    [all_true_sig_true, all_true_sig_false] = Annex.predict(seq, [1.0, 1.0, 1.0])
+    [should_be_true] = Annex.predict(seq, [1.0, 1.0, 1.0])
+    [should_be_false] = Annex.predict(seq, [1.0, 0.0, 1.0])
 
-    assert_in_delta(all_true_sig_false, 0.0, 0.1)
-    assert_in_delta(all_true_sig_true, 1.0, 0.1)
+    assert_in_delta(should_be_false, 0.0, 0.1)
+    assert_in_delta(should_be_true, 1.0, 0.1)
   end
 end
