dc2d55225d8e94c6ef77c905e0f0958d274cb4ce
Jason Goldberger <jlgoldb2@asu.edu>
2019-05-13 19:55:23 -0700

    made repo public, added annex to hex.pm

--- mix.exs
+++ mix.exs
@@ -8,7 +8,12 @@ defmodule Annex.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env())
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Annex",
+      source_url: "https://github.com/elbow-jason/annex"
     ]
   end
 
@@ -25,7 +30,23 @@ defmodule Annex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_csv, "~> 0.3", only: [:dev]}
+      {:nimble_csv, "~> 0.3", only: [:dev]},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "A composable deep learning framework in Elixir"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "annex",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE CHANGELOG.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/elbow-jason/annex"}
     ]
   end
 end
