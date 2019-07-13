e719b166af2033abe75a302a8d886b7c43d1bf20
Jason Goldberger <jlgoldb2@asu.edu>
2019-07-13 12:50:29 -0700

    readded xor2 test

--- test/xor2_test.exs
+++ test/xor2_test.exs
@@ -1,46 +1,46 @@
-# defmodule Annex.SequenceXor2Test do
-#   use ExUnit.Case, async: true
-#   alias Annex.Layer.Sequence
+defmodule Annex.SequenceXor2Test do
+  use ExUnit.Case, async: true
+  alias Annex.Layer.Sequence
 
-#   test "xor2 test" do
-#     data = [
-#       [0.0, 0.0],
-#       [0.0, 1.0],
-#       [1.0, 0.0],
-#       [1.0, 1.0]
-#     ]
+  test "xor2 test" do
+    data = [
+      [0.0, 0.0],
+      [0.0, 1.0],
+      [1.0, 0.0],
+      [1.0, 1.0]
+    ]
 
-#     labels = [
-#       [0.0, 1.0],
-#       [1.0, 0.0],
-#       [1.0, 0.0],
-#       [0.0, 1.0]
-#     ]
+    labels = [
+      [0.0, 1.0],
+      [1.0, 0.0],
+      [1.0, 0.0],
+      [0.0, 1.0]
+    ]
 
-#     {:ok, _output, %Sequence{} = seq} =
-#       Annex.sequence([
-#         Annex.dense(11, 2),
-#         Annex.activation(:relu),
-#         Annex.dense(2, 11),
-#         Annex.activation(:sigmoid)
-#       ])
-#       |> Annex.train(data, labels,
-#         name: "xor2",
-#         learning_rate: 0.02,
-#         halt_condition: {:epochs, 200_000}
-#       )
+    {:ok, %Sequence{} = seq, _output} =
+      Annex.sequence([
+        Annex.dense(11, 2),
+        Annex.activation(:relu),
+        Annex.dense(2, 11),
+        Annex.activation(:sigmoid)
+      ])
+      |> Annex.train(data, labels,
+        name: "xor2",
+        learning_rate: 0.02,
+        halt_condition: {:epochs, 60_000}
+      )
 
-#     [pred_yes, pred_no] = Annex.predict(seq, [0.0, 0.0])
-#     assert_in_delta(pred_yes, 0.0, 0.1)
-#     assert_in_delta(pred_no, 1.0, 0.1)
-#     [pred_yes, pred_no] = Annex.predict(seq, [0.0, 1.0])
-#     assert_in_delta(pred_yes, 1.0, 0.1)
-#     assert_in_delta(pred_no, 0.0, 0.1)
-#     [pred_yes, pred_no] = Annex.predict(seq, [1.0, 0.0])
-#     assert_in_delta(pred_yes, 1.0, 0.1)
-#     assert_in_delta(pred_no, 0.0, 0.1)
-#     [pred_yes, pred_no] = Annex.predict(seq, [1.0, 1.0])
-#     assert_in_delta(pred_yes, 0.0, 0.1)
-#     assert_in_delta(pred_no, 1.0, 0.1)
-#   end
-# end
+    [pred_yes, pred_no] = Annex.predict(seq, [0.0, 0.0])
+    assert_in_delta(pred_yes, 0.0, 0.1)
+    assert_in_delta(pred_no, 1.0, 0.1)
+    [pred_yes, pred_no] = Annex.predict(seq, [0.0, 1.0])
+    assert_in_delta(pred_yes, 1.0, 0.1)
+    assert_in_delta(pred_no, 0.0, 0.1)
+    [pred_yes, pred_no] = Annex.predict(seq, [1.0, 0.0])
+    assert_in_delta(pred_yes, 1.0, 0.1)
+    assert_in_delta(pred_no, 0.0, 0.1)
+    [pred_yes, pred_no] = Annex.predict(seq, [1.0, 1.0])
+    assert_in_delta(pred_yes, 0.0, 0.1)
+    assert_in_delta(pred_no, 1.0, 0.1)
+  end
+end
