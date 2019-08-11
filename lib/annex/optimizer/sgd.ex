3a2cadccbfd3c76f07b88a76ac103e805dde0038
Jason Goldberger <jlgoldb2@asu.edu>
2019-08-11 07:57:40 -0700

    added stochastic gradient descent optimizer

--- lib/annex/optimizer/sgd.ex
+++ lib/annex/optimizer/sgd.ex
@@ -10,6 +10,8 @@ defmodule Annex.Optimizer.SGD do
     Layer.Backprop
   }
 
+  import Annex.Utils, only: [is_pos_integer: 1]
+
   def train(%{} = learner, dataset, opts) do
     cond do
       Layer.is_layer?(learner) ->
@@ -18,16 +20,13 @@ defmodule Annex.Optimizer.SGD do
   end
 
   defp train_layer(orig_layer, dataset, opts) do
-    batch_size = Keyword.get(opts, :batch_size, 1)
     cost = Keyword.get_lazy(opts, :cost, fn -> Defaults.get_defaults(:cost) end)
 
+    batch_size = Keyword.get(opts, :batch_size)
     batched_dataset = batch_dataset(dataset, batch_size)
 
     Enum.reduce(batched_dataset, orig_layer, fn {inputs, labels}, layer1 ->
       {%{} = layer2, prediction} = Layer.feedforward(layer1, inputs)
-
-      prediction = Data.to_flat_list(prediction)
-      labels = Data.to_flat_list(labels)
 
       error = Data.error(prediction, labels)
 
@@ -48,7 +47,11 @@ defmodule Annex.Optimizer.SGD do
     end)
   end
 
-  def batch_dataset(dataset, batch_size) do
+  def batch_dataset(dataset, nil) do
+    dataset
+  end
+
+  def batch_dataset(dataset, batch_size) when is_pos_integer(batch_size) do
     dataset
     |> Enum.shuffle()
     |> Enum.take(batch_size)
