afad5827a4ff4a935657903f3028ff5a5a8239d2
Jason Goldberger <jlgoldb2@asu.edu>
2019-08-11 20:07:25 -0700

    removed commented code from annex.ex

--- lib/annex.ex
+++ lib/annex.ex
@@ -22,37 +22,14 @@ defmodule Annex do
   end
 
   @doc """
-  Given a frequency (between `0.0` and `1.0`) returns a `Dropout` layer that
-  randomly, at the given frequency, returns `0.0` for an input regardless of
-  that input's value.
+  Given a frequency (between `0.0` and `1.0`) returns a LayerConfig for a `Dropout`.
+  The `Dropout` layer randomly, at a given frequency, returns `0.0` for an input
+  regardless of that input's value.
   """
   @spec dropout(float()) :: LayerConfig.t(Dropout)
   def dropout(frequency) do
     LayerConfig.build(Dropout, frequency: frequency)
   end
-
-  # @doc """
-  # Given a `Learner` or `Layer` behaviour implementing struct initializes the
-  # struct.
-
-  # If the given struct is both a `Learner` and a `Layer` calling `initialize/2`
-  # will call `Learner.init_learner/2` before `Layer.init_layer/2` .
-
-  # If the given struct is not a `Learner` nor a `Layer` this function returns
-  # an error tuple: `{:error, :invalid_model}`.
-  # """
-  # @spec initialize(struct()) :: struct()
-  # def initialize(model, opts \\ []) do
-  #   cond do
-  #     is_learner?(model) -> Learner.init_learner(model, opts)
-  #     is_layer_config?(model) -> LayerConfig.init_layer(model)
-  #     is_layer?(model) -> model
-  #   end
-  # end
-
-  # defp is_layer_config?(cfg), do: match?(%LayerConfig{}, cfg)
-  # defp is_layer?(layer), do: Layer.is_layer?(layer)
-  # defp is_learner?(learner), do: Learner.is_learner?(learner)
 
   @doc """
   Given a number of `rows`, `columns`, some `weights`,
@@ -74,22 +51,6 @@ defmodule Annex do
   def dense(rows, columns) do
     LayerConfig.build(Dense, rows: rows, columns: columns)
   end
-
-  # @doc """
-  # Given a number of `rows` returns a Dense layer.
-
-  # Without the `columns` of `dense/2` the `columns` of the returned Dense layer
-  # must be inferred during initialization (`init_layer/2`) from a previous
-  # layer's number of outputs.
-
-  # Without the `weights` and `biases` of `dense/4` this Dense layer will be
-  # have no neurons. Upon `Layer.init_layer/2` the Dense layer will be
-  # initialized with random neurons; Neurons with random weights and biases.
-  # """
-  # @spec dense(pos_integer()) :: LayerConfig.t(Dense)
-  # def dense(rows) do
-  #   LayerConfig.build(Dense, rows: rows)
-  # end
 
   @doc """
   Given an Activation's name returns appropriate `Activation` layer.
